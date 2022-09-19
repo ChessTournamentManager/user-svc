ccf04e1f9f114fec4a37a79e6a43b499969c5468
Judah <i460958@fhict.nl>
2022-09-19 14:46:17 +0200

    Added CI file

--- src/test/java/com/chesstournamentmanager/usersvc/UserSvcApplicationTests.java
+++ src/test/java/com/chesstournamentmanager/usersvc/UserSvcApplicationTests.java
@@ -1,13 +1,22 @@
 package com.chesstournamentmanager.usersvc;
 
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserSvcApplicationTests {
 
+	@Autowired
+	private UserSvcApplication userSvcApplication;
+
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void sendsWelcomeMessage() {
+		assertThat(userSvcApplication.getMessage()).isEqualTo("Welcome to the user service.");
+	}
 }
